ff1d9497584913d82908d2550a41d07e94634515
Laknath <blaknath@gmail.com>
2012-09-05 08:24:18 +0530

    Changed original terminologies   - bound_to -> use_scope_in     - options :by -> :to     - options :scope_function -> :using   - bound_by -> add_scope

--- lib/boundary/controller.rb
+++ lib/boundary/controller.rb
@@ -3,42 +3,42 @@ module Boundary
     # Adds a scope helper that call the model with a given parameter (ie: subdomain)
     #
     # Options:
-    # * <tt>:by</tt> - Target scope model name (default: :company)
-    # * <tt>:scope_function</tt> - Name of a function that returns the target scope object. This object should contain an ID column that matches with the foreign_id column of the scoped object (default: :current_company)
+    # * <tt>:to</tt> - Target scope model name (default: :company)
+    # * <tt>:using</tt> - A function that returns the object used for scoping. This object should contain an ID column that matches with the foreign_id column of the scoped object (default: :current_company)
     #
-    # bound_to :subscription, :by => :account, :scope_function => :current_employee
+    # use_scope_in :subscription, :to => :account, :using => :current_employee
     #
     # In your actions,
     #
     # def show
-    #   @subscription = bound_by_account { Subscription.find(params) }
+    #   @subscription = scoped_by_account { Subscription.find(params) }
     #   ...
     # end
     #
     # For multiple scopes,
     #
-    # bound_to :subscription, :by => :account, :scope_function => :current_employee
-    # bound_to :transaction, :by => :account, :scope_function => :current_employee
+    # use_scope_in  :subscription, :to => :account, :using => :current_employee
+    # use_scope_in  :transaction, :to => :account, :using => :current_employee
     #
     # def show
-    #   @subscription = subscription_bound_by_account { Subscription.find(params) }
-    #   @transaction  = transaction_bound_by_account { Transaction.find(params) }
+    #   @subscription = subscription_scoped_by_account { Subscription.find(params) }
+    #   @transaction  = transaction_scoped_by_account { Transaction.find(params) }
     # end
-    def bound_to(model, *args, &block)
+    def use_scope_in(model, *args, &block)
       options = args.extract_options!
       
       model_name  = model.to_s.camelize
-      options[:by]            ||= :company
-      options[:scope_function]  ||= "current_#{options[:by]}"
+      options[:to]      ||= :company
+      options[:using]   ||= "current_#{options[:to]}"
 
-      helper_name   = "#{model}_bound_by_#{options[:by]}"
+      helper_name   = "#{model}_scoped_by_#{options[:to]}"
 
       self.class_eval <<-"end_eval", __FILE__, __LINE__
           def #{helper_name}(&block)
-            #{model_name}.bound_by_#{options[:by]}(#{options[:scope_function]}.id, &block)
+            #{model_name}.scoped_by_#{options[:to]}(#{options[:using]}.id, &block)
           end
           
-          alias_method :bound_by_#{options[:by]}, :#{helper_name}
+          alias_method :scoped_by_#{options[:to]}, :#{helper_name}
       end_eval
     end
   end
--- lib/boundary/model.rb
+++ lib/boundary/model.rb
@@ -5,16 +5,16 @@ module Boundary
     # Options:
     # * <tt>:foreign_id</tt> - Foreign ID column name (default: :company_id)
     #
-    # bound_by :account, :foreign_id => :employee_id
+    # add_scope :account, :foreign_id => :employee_id
     #
-    # Subscription.bound_by_account(account_id) { query }
-    def bound_by(scope, *args)
+    # Subscription.scoped_by_account(account_id) { ... }
+    def add_scope(scope, *args)
       options = args.extract_options!
 
       options[:foreign_id]  ||= :company_id
 
       self.class_eval <<-"end_eval", __FILE__, __LINE__
-          def self.bound_by_#{scope}(foreign_id, *args)
+          def self.scoped_by_#{scope}(foreign_id, *args)
             self.where(:#{options[:foreign_id]} => foreign_id).scoping do
               yield if block_given?
             end
